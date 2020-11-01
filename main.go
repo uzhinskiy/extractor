a3a9e04016d2bdd3d739a9565af3840e11585e21
Boris <boris.uzhinsky@flant.com>
2020-11-01 23:48:41 +0300

    front is ready

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+        "log"
+        "mime"
+        "net/http"
+        "os"
+        "path"
+        "strings"
+)
+
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+
+	file := r.URL.Path
+	if file == "/" {
+		file = "/index.html"
+	}
+	cFile := strings.Replace(file, "/", "", 1)
+  data, err := Asset(cFile)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(r.RemoteAddr, "\t", r.Method, "\t", r.URL.Path, "\t", r.UserAgent())
+	/* отправить его клиенту */
+	contentType := mime.TypeByExtension(path.Ext(cFile))
+	w.Header().Set("Content-Type", contentType)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Server", "gohttp/0.2")
+	w.Write(data)
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+
+  
+}
+
+func main() {
+        http.HandleFunc("/", staticHandler)
+        http.HandleFunc("/api/", apiHandler)
+        http.ListenAndServe(":9400", nil)
+}
